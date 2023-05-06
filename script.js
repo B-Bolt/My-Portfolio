5befc71d21e2a69cd978d6f2489a4ef8c3b3064b
B-Bolt <125896095+B-Bolt@users.noreply.github.com>
2023-05-06 23:28:23 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -20,43 +20,88 @@ const loadMore = document.querySelector('.Load-More');
 const s1El = document.querySelector('.S1');
 const divProducts = document.querySelectorAll('.W-All');
 const dElement = document.querySelectorAll('.D-Element');
+const counter1El = document.querySelectorAll('.counter');
+const frowF = document.querySelectorAll('.F-Row').forEach((frow) => frow.style.flexBasis);
+const menuImg = document.querySelector('#navImg');
+const menuUl = document.getElementById('navId');
+const navbarEl = document.querySelector('.Navbar');
+const flipElp = document.querySelector('#Flip-P');
+const slideEl = document.getElementsByClassName('CC');
+let slideIndex = 1;
 
 // Function's
 
-mySkills.addEventListener('mouseover', function () {
+const rowMax = function () {
     fRow.forEach((frow) => frow.style.flexBasis = '100%');
-    mySkills.addEventListener('mouseleave', function () {
-        fRow.forEach((frow) => frow.style.flexBasis = frow.style.flexBasis = '30%');
-    })
+    counter1El.forEach((counter) => counter.textContent = '');
+}
+// const counterPlus = function () {
+//     for (let i = 0; i < f1Row.style.flexBasis.length; i++) {
+//         fRow.forEach((frow) => frow.style.flexBasis = '100%');
+//     }
+// }
+
+// Navbar Menu - For Mobile
+
+menuImg.addEventListener('click', function () {
+    menuImg.style.transition = 'all 0.6s';
+    menuUl.style.transition = 'all 2s';
+    if (menuUl.style.display === 'block' && menuImg.style.transform === 'translateX(-200px)') {
+        menuImg.style.transform = 'translateX(0)';
+        menuUl.style.display = 'none';
+    } else {
+        menuImg.style.transform = 'translateX(-200px)';
+        menuUl.style.display = 'block';
+    }
 });
-
-// Set DOM Elements
-
-f1Row.style.flexBasis = '0%';
-fRow.forEach((frow) => frow.style.flexBasis = '0');
-loadMore.classList.add('hidden');
 
 // NavBar Link's 
 
 navLinkAll.forEach((item) => item.addEventListener('click', function () {
     item.classList.toggle('link-active');
+    navbarEl.addEventListener('mouseleave', function () {
+        item.classList.remove('link-active');
+    })
 }));
+
+
+// Skills
+
+mySkills.addEventListener('mouseover', function () {
+    rowMax();
+    mySkills.addEventListener('mouseleave', function () {
+        fRow.forEach((frow) => frow.style.flexBasis = '');
+    })
+}, { once: true });
+
+// Set DOM Elements
+
+fRow.forEach((frow) => frow.style.flexBasis = '0');
+loadMore.classList.add('hidden');
 
 // For Load More Button
 
 loadButton.addEventListener('click', function () {
     loadMore.classList.toggle('hidden');
+    loadButton.style.transition = "all 2s";
+    loadButton.value === 'Load More Work' ? loadButton.value = 'Close More Work' : loadButton.value = 'Load More Work';
 });
 
+// For Work's Flip
 
-// For Professional Skills
+divProducts.forEach((W) => W.addEventListener('mouseover', function () {
+    W.flipElp.textContent = 'For More info , Click Below';
+}));
 
-zRow.addEventListener('mouseover', function () {
-    f1Row.style.flexBasis = '100%';
-    f1Row.addEventListener('mouseleave', function () {
-        f1Row.style.flexBasis = '10%';
-    })
-});
+
+// For Professional Skills - old Code
+
+// zRow.addEventListener('mouseover', function () {
+//     f1Row.style.flexBasis = '100%';
+//     f1Row.addEventListener('mouseleave', function () {
+//         f1Row.style.flexBasis = '10%';
+//     })
+// });
 
 // for Work Done
 
@@ -73,11 +118,27 @@ zRow.addEventListener('mouseover', function () {
     })
 })); */
 
-divProducts.forEach((products) => products.addEventListener('mouseover', function () {
-    divProducts.forEach((products) => products.classList.remove('visual'));
-    products.classList.toggle('visual');
-    divProducts.forEach((products) => products.addEventListener('mouseleave', function () {
-        products.classList.remove('visual');
-    })
-    )
-}));
+// divProducts.forEach((products) => products.addEventListener('mouseover', function () {
+//     divProducts.forEach((products) => products.classList.remove('visual'));
+//     products.classList.toggle('visual');
+//     divProducts.forEach((products) => products.addEventListener('mouseleave', function () {
+//         products.classList.remove('visual');
+//     })
+//     )
+// }));
+
+// Slideshow
+
+function plusS(n) {
+    showSlide(slideIndex += n)
+}
+showSlide(slideIndex);
+
+function showSlide(n) {
+    if (n > slideEl.length) slideIndex = 1;
+    if (n < 1) slideIndex = slideEl.length;
+    for (let i = 0; i < slideEl.length; i++) {
+        slideEl[i].style.display = 'none';
+    }
+    slideEl[slideIndex - 1].style.display = 'block';
+}
