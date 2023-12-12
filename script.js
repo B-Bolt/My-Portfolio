6f541155c166df5e0cf40b8068a56a0a26c378c1
B-Bolt <125896095+B-Bolt@users.noreply.github.com>
2023-12-12 17:28:28 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const ulDropDownLi = document.querySelectorAll('.ul-DDli');
 const subDropDownLi = document.querySelectorAll('.ul-sDDli');
 let slideIndex = 1;
 
-// Function's
+// ---------- Function's ----------
 
 const rowMax = function () {
     fRow.forEach((frow) => frow.style.flexBasis = '100%');
@@ -51,8 +51,28 @@ const rowMax = function () {
 //     }
 // }
 
+//
 
-// Navbar Menu - For Mobile
+// --------------- Begin Header TypeWriter ----------
+
+let iC = 0;
+
+let exText = 'به‌خاطر انبارگردانی تمام اجناس تولیدی با 10% تخفیف عرضه می‌شوند';
+
+let exParagraph = document.getElementById('exParagraph');
+
+function typeWriter() {
+    if (iC < exText.length) {
+        exParagraph.innerHTML += exText.charAt(iC);
+        iC++;
+        setTimeout(typeWriter, 40);
+    }
+};
+typeWriter();
+// ---------- End Header TypeWriter ----------
+
+
+// ---------- Navbar Menu - For Mobile ----------
 
 
 menuImg.addEventListener('click', function () {
@@ -68,9 +88,9 @@ menuImg.addEventListener('click', function () {
     }
 });
 
-// End Navbar Menu - For Mobile
+// ---------- End Navbar Menu - For Mobile ----------
 
-// NavBar Link's 
+// ---------- NavBar Link's ----------
 
 navLinkAll.forEach((item) => item.addEventListener('click', function () {
     item.classList.toggle('link-active');
@@ -79,8 +99,79 @@ navLinkAll.forEach((item) => item.addEventListener('click', function () {
     })
 }));
 
+// ---------- End Navbar Link's ----------
 
-// Skills
+// ---------- Begin the Modal ----------
+
+function openModal() {
+    document.getElementById("slideModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("slideModal").style.display = "none";
+}
+
+let slideindexX = 1;
+showSlides(slideindexX);
+
+// ---------- Next/previous controls ----------
+
+function plusSlides(n) {
+    showSlides(slideindexX += n);
+}
+
+// ---------- Thumbnail image controls ----------
+
+function currentSlide(n) {
+    showSlides(slideindexX = n);
+}
+
+// need to be change later
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("widePics");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideindexX = 1 }
+    if (n < 1) { slideindexX = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideindexX - 1].style.display = "block";
+    dots[slideindexX - 1].className += " active";
+    captionText.innerHTML = dots[slideindexX - 1].alt;
+}
+
+// ---------- End the Modal ----------
+
+// ---------- Begin of Slideshow ----------
+
+function plusS(n) {
+    showSlide(slideIndex += n)
+}
+showSlide(slideIndex);
+
+function showSlide(n) {
+    if (n > slideEl.length) slideIndex = 1;
+    if (n < 1) slideIndex = slideEl.length;
+    for (let i = 0; i < slideEl.length; i++) {
+        slideEl[i].style.display = 'none';
+    }
+    slideEl[slideIndex - 1].style.display = 'block';
+};
+
+// ---------- End of Slideshow ----------
+
+// ---------- Skill's ----------
+
+// mySkills.onscroll(function () {
+//     rowMax();
+//     setTimeout(fRow.forEach((frow) => frow.style.flexBasis = ''), 3000);
+// });
 
 mySkills.addEventListener('mouseover', function () {
     rowMax();
@@ -89,12 +180,14 @@ mySkills.addEventListener('mouseover', function () {
     })
 }, { once: true });
 
-// Set DOM Elements
+// ---------- End of Skill's ----------
+
+// ---------- Set DOM Elements ----------
 
 fRow.forEach((frow) => frow.style.flexBasis = '0');
 loadMore.classList.add('hidden');
 
-// For Load More Button
+// ---------- For Load More Button ----------
 
 loadButton.addEventListener('click', function () {
     loadMore.classList.toggle('hidden');
@@ -102,13 +195,13 @@ loadButton.addEventListener('click', function () {
     loadButton.value === 'دیدن نمونه های بیشتر' ? loadButton.value = 'بستن' : loadButton.value = 'دیدن نمونه های بیشتر';
 });
 
-// For Work's Flip
+// ---------- For Work's Flip ----------
 
 // divProducts.forEach((W) => W.addEventListener('mouseover', function () {
 //     W.flipElp.textContent = 'For More info , Click Below';
 // }));
 
-// Flip Section
+// ---------- Flip Section ----------
 
 wAll.forEach((wall) => wall.addEventListener('mouseover', function () {
     if (wall.contains('flipImage') || ('flipSection')) {
@@ -116,35 +209,21 @@ wAll.forEach((wall) => wall.addEventListener('mouseover', function () {
     }
 }))
 
-// End Flip Section
+// ---------- End Flip Section ----------
 
-// For Nav Drop Down
+// ---------- For Nav Drop Down ----------
 
 
-// End Nav Drop Down
+// ---------- End Nav Drop Down ----------
 
-// Begin sub Drop Down
-let once = true;
+// ---------- Begin D Work Count Up ----------
 
-const fullWidth = function () {
-    ulDropDownLi.forEach((ulli) => ulli.addEventListener('mouseover', function () {
-        ulDropDownLi.forEach((ulli) => ulli.style.display = 'none');
-        ulDropDownLi.forEach(this.style.display = 'block', this.style.width = '95%');
-    }));
-    ulDropDown.addEventListener('mouseleave', function () {
-        ulDropDownLi.forEach((ulli) => ulli.style.display = '');
-        if (ulDropDownLi.forEach((ulli) => ulli.style.width = '')) {
-            ulDropDownLi.forEach((ulli) => ulli.style.flex = '');
-            ulDropDownLi.forEach(this.style.display = '', this.style.width = '');
-        }
-    })
-}
 
-fullWidth();
+// ---------- End D Work Count Up ----------
 
-// End sub Drop Down
 
-// For Professional Skills - old Code
+
+// ---------- For Professional Skills - old Code ----------
 
 // zRow.addEventListener('mouseover', function () {
 //     f1Row.style.flexBasis = '100%';
@@ -153,13 +232,13 @@ fullWidth();
 //     })
 // });
 
-// for Work Done
+// ---------- for Work Done ----------
 
 /* dElement.forEach((elements) => elements.addEventListener('mouseover', function () {
     elements.classList.toggle('D-Active');
 })); */
 
-// For Product Div
+// ---------- For Product Div ----------
 
 /* divProducts.forEach((products) => products.addEventListener('mouseover', function () {
     products.classList.toggle('visual');
@@ -177,22 +256,6 @@ fullWidth();
 //     )
 // }));
 
-// Slideshow
-
-function plusS(n) {
-    showSlide(slideIndex += n)
-}
-showSlide(slideIndex);
-
-function showSlide(n) {
-    if (n > slideEl.length) slideIndex = 1;
-    if (n < 1) slideIndex = slideEl.length;
-    for (let i = 0; i < slideEl.length; i++) {
-        slideEl[i].style.display = 'none';
-    }
-    slideEl[slideIndex - 1].style.display = 'block';
-}
-
 // Input Contact Section
 
 ynButton.addEventListener('click', function () {
@@ -200,4 +263,4 @@ ynButton.addEventListener('click', function () {
     ynButton.addEventListener('focusout', function () {
         ynButton.placeholder = 'Your Name';
     })
-})
+});
